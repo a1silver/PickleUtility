d7877b413b591eb945de82130bc1c056730739cc
minecraftpr03 <47282021+MasterCoder21@users.noreply.github.com>
2022-12-02 17:25:52 +0000

    Random updates

--- index.mjs
+++ index.mjs
@@ -2,14 +2,34 @@ import Server from 'bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
-
 const bare =  new Server('/bare/', '');
 const serve = new nodeStatic.Server('static/');
 
 const server = http.createServer();
 
+function isUrl(val = ''){
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
+
 server.on('request', (request, response) => {
     if (bare.route_request(request, response)) return true;
+    let url = new URL(request.url, `http://${request.headers.host}`);
+    if(url.pathname === 'search') {
+      let query = url.search.substring(3);
+      window.navigator.serviceWorker.register('./static/sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = query.trim();
+      
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+      return;
+    }
     serve.serve(request, response);
 });
 
